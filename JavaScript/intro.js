a8bafc6a35a7ba3faf2c40481689532b726c4f10
Evan Granato <evgranato@gmail.com>
2020-06-15 13:21:05 -0500

    update

---
+++ JavaScript/intro.js
@@ -0,0 +1,8 @@
+let userFirstName = prompt("What is your first name?");
+let userLastName = prompt("What's your last name?");
+let userAge = prompt("How old are you?");
+
+let fullName = userFirstName + " " + userLastName;
+
+console.log("Nice to meet you, " + fullName);
+console.log("You are " + userAge +" years old.")
