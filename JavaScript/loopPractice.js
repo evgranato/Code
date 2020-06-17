eb64b571dfdfed549ce38535bd3eb85459e1e66a
Evan Granato <evgranato@gmail.com>
2020-06-17 17:36:15 -0500

    update

--- JavaScript/loopPractice.js
+++ JavaScript/loopPractice.js
@@ -5,11 +5,18 @@
 //     num +=2;
 // }
 
-let num = 1;
+// let num = 1;
 
-while(num <=20) {
-    if(num % 4 ===0){
-    console.log(num);
-}
-num++;
+// while(num <=20) {
+//     if(num % 4 ===0){
+//     console.log(num);
+// }
+// num++;
+// }
+
+let count = 10;
+
+while(count < 20) {
+    console.log(count);
+    count++
 }
