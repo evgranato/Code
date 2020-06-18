e79ce4b2e12f3037fd845864af4ff8ec2f5c1d49
Evan Granato <evgranato@gmail.com>
2020-06-17 19:47:42 -0500

    update

---
+++ JavaScript/annoyOmatic.js
@@ -0,0 +1,17 @@
+// var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" answer !== "yeah") {
+//     var answer = prompt("Are we there yet?")
+// }
+
+// alert("YAY, WE MADE IT!!");
+
+//VERSION 2
+
+var answer = prompt("Are we there yet?");
+
+while(answer.indexOf("yes") === -1) {
+    var answer = prompt("Are we there yet?")
+}
+
+alert("YAY, WE MADE IT!!");
--- JavaScript/loopPractice.js
+++ JavaScript/loopPractice.js
@@ -14,9 +14,36 @@
 // num++;
 // }
 
-let count = 10;
+// let count = -10;
 
-while(count < 20) {
-    console.log(count);
-    count++
-}
+// while(count < 20) {
+//     console.log(count);
+//     count++
+// }
+
+// let count = 10;
+
+// while(count< 41) {
+//     if (count % 2 == 0) {
+//     console.log(count) ;
+// }
+//     count++;
+// }
+
+// let count = 300;
+
+// while(count< 334) {
+//     if (count % 2 != 0) {
+//     console.log(count) ;
+// }
+//     count++;
+// }
+
+// let count = 5;
+
+// while(count< 51) {
+//     if (count % 5 == 0 && count % 3 == 0) {
+//     console.log(count) ;
+// }
+//     count++;
+// }
