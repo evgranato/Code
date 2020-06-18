53e0b93f82c435767c7eb7ad9df789878cc4c2cc
Evan Granato <evgranato@gmail.com>
2020-06-18 11:31:01 -0500

    update

---
+++ JavaScript/forLoops.js
@@ -0,0 +1,45 @@
+// for (Intit; condition; stop) {
+//     //run some code
+// }
+
+// for (let count = 0; count < 6; count++) {
+//     console.log(count);
+// }
+
+// let str = "hello";
+
+// for(let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+// for(let i = 0; i < 16; i+=8) {
+//     console.log(i)
+// }
+
+// let str = "ahceclwlxo";
+
+// for(let i = 1; i < str.length; i+=2) {
+//     console.log(str[i]);
+// }
+
+// for(let i = -10; i < 20; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 10; i < 41; i++) {
+//     if(i % 2 ===0) {
+//     console.log(i);
+//     }
+// }
+
+for(let i = 300; i < 334; i++) {
+    if(i % 2 !== 0) {
+    console.log(i);
+    }
+}
+
+for(let i = 5; i < 51; i++) {
+    if (i % 5 ===0 && i % 3 ===0) {
+    console.log(i);
+    }
+}
