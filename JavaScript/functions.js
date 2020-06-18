3605686d75e35a7afc3784f58e8378d3b6737e26
Evan Granato <evgranato@gmail.com>
2020-06-18 15:46:21 -0500

    update

---
+++ JavaScript/functions.js
@@ -0,0 +1,16 @@
+// function doSomething() {
+//     console.log("Hello World")
+// }
+// doSomething();
+
+// function sayHi() {
+//     console.log("HELLO!");
+//     console.log("GOODBYE!")
+// }
+// sayHi();
+
+let num = Number(prompt("Give me a number"));
+function square(num) {
+    console.log(num * num);
+}
+square(num)
