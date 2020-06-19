42eb4d6afcb96f298b77dafb32e70386d2f084dd
Evan Granato <evgranato@gmail.com>
2020-06-19 15:20:00 -0500

    update

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -19,9 +19,48 @@
 //     return x * x;
 // }
 
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+// function capitalize(str) {
+//     return str.charAt(0).toUpperCase() + str.slice(1);
+// }
 
 // let city = "paris";
 // let capital = capitalize(city);
+
+// function test(x,y) {
+//     return y - x;
+// }
+
+// function test(x) {
+//     return x*2;
+//     console.log(x);
+//     return x/2;
+// }
+
+// IS EVEN: 
+
+// function isEven(num) {
+//     if (num % 2 === 0) {
+//         return true;
+//     }
+//     return false;
+// }
+
+// FACTORIAL:
+
+// function factorial(num) {
+//     if(num ===0) {
+//         return 1;
+//     }
+//     var result = num;
+//     for(let i = num - 1; i >= 1; i--) {
+//         result = result * i;
+//     }
+//     return result;
+// }
+
+// kebabToSnake:
+
+// function kebabToSnake(str) {
+//     let result = str.replace(/-/g, "_");
+//     return result;
+// }
