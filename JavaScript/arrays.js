f518597931f2718f3c3ec6e2a5400d6e2ea45372
Evan Granato <evgranato@gmail.com>
2020-06-20 10:01:16 -0500

    update

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,2 +1,21 @@
-let friends = ["Charlie", "Liz", "David", "Mattias"]
+// window.setTimeout(function() {
+//     // put all of your JS code from the lecture here
+//   }, 500);
+let todo = [];
+
+let startTodo = prompt("What would you like to do?");
+
+while(startTodo !== "quit") {
+if(startTodo === "new") {
+   let nTodo = prompt("New Todo: ");
+   todo.push(nTodo);
+}
+else if(startTodo === "list") {
+   alert(todo);
+}
+startTodo = prompt("What would you like to do?");
+}
+alert("Ok, You Quit The App");
+
+
 
