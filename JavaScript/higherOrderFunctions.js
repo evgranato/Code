de66b49392227993fd672ac35afb193e3fa49ae1
Evan Granato <evgranato@gmail.com>
2020-06-20 06:17:49 -0500

    update

---
+++ JavaScript/higherOrderFunctions.js
@@ -0,0 +1,15 @@
+// function sing() {
+//     console.log("Twinkle, twinkle...");
+//     console.log("Little star...");
+// }
+
+// setInterval(sing,1000)
+
+// OR YOU CAN PASS THE FUNCTION INTO THE FUNCTION, BUT IT'S ONLY IN THE FUNCTION, NOT ACCESSABLE AGAIN
+
+// setInterval(function sing() {
+//     console.log("Twinkle, twinkle...");
+//     console.log("Little star 2");
+// }, 2000);
+
+// USE clearInterval() with the number given when you run the inteval code
