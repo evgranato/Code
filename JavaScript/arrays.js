3b42e6f5c24f5c0c024abe74266f68909fca6c94
Evan Granato <evgranato@gmail.com>
2020-06-22 08:55:53 -0500

    update

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -1,21 +1,41 @@
-// window.setTimeout(function() {
-//     // put all of your JS code from the lecture here
-//   }, 500);
-let todo = [];
+window.setTimeout(function() {
+   //  put all of your JS code from the lecture here
+
+let todos = [];
 
 let startTodo = prompt("What would you like to do?");
 
 while(startTodo !== "quit") {
-if(startTodo === "new") {
-   let nTodo = prompt("New Todo: ");
-   todo.push(nTodo);
+   if(startTodo === "new") {
+      addTodo();
 }
-else if(startTodo === "list") {
-   alert(todo);
+   else if(startTodo === "list") {
+      listTodos();
+}
+   else if(startTodo === "delete") {
+      deleteTodo();
 }
 startTodo = prompt("What would you like to do?");
 }
 alert("Ok, You Quit The App");
 
+function listTodos() {
+   console.log("***********");
+   todos.forEach(function(todo, index) {
+   alert(index + ": " + todo);
+   })
+   }
+function addTodo() {
+   let nTodo = prompt("New Todo: ");
+   todos.push(nTodo);
+   alert(nTodo + " added to your list!");
+}
 
+function deleteTodo() {
+   let itemNum = prompt("What index Todo would you like to delete?");
+   todos.splice(itemNum, 1);
+   alert("Deleted Todo");
+}
+
+}, 500);
 
