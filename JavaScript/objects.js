49a969f59f0f365c4cd7d2234381744ab4eea96d
Evan Granato <evgranato@gmail.com>
2020-06-26 12:26:43 -0500

    update

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -1,6 +1,38 @@
-var person = {
-    name: "Cindy",
-    age: 32,
-    city: "Missoula"
-};
+// var person = {
+//     name: "Cindy",
+//     age: 32,
+//     city: "Missoula"
+// };
 
+// var posts = [
+//     {
+//         title: "Cats are mediocre",
+//         author: "Colt",
+//         comments: ["Awesome post", "terrible post"]
+//     },
+//     {
+//         title: "Cats are actually awesome",
+//         author: "Cat Luvr",
+//         comments: ["<3", "Go to hell"]
+//     }
+// ]
+
+// posts[0].comments.forEach(function(post) {
+//     console.log(post);
+// })
+// var test = []
+// posts.forEach(function(post) {
+//     test += post.comments + ",";
+// })
+
+// EXERCISES:
+
+var someObject = {
+    friends: [
+        {name: "Malfoy"},
+        {name: "Crabbie"},
+        {name: "Goyle"}
+],
+color: "baby blue",
+isEvil: true
+};
