248a4b8caf7574fe98fc8e19344bc65470972818
Evan Granato <evgranato@gmail.com>
2020-06-28 13:32:10 -0500

    update

---
+++ DOM/exercise1.js
@@ -0,0 +1,7 @@
+var tag1 = document.getElementById("first")
+
+var tag2 = document.getElementsByClassName("special")[0]
+
+var tag3 = document.querySelector("#first")
+
+var tag4 = document.querySelector(".special")
