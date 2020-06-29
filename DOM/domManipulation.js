9f6b406dc0191ff432eb14a04f316d15d16540f5
Evan Granato <evgranato@gmail.com>
2020-06-29 15:24:45 -0500

    update

---
+++ DOM/domManipulation.js
@@ -0,0 +1,28 @@
+//SELECT
+// var tag = document.getElementById("highlight");
+
+// //MANIPULATE
+// tag.style.color = "blue";
+// tag.style.border = "10px solid red";
+// tag.style.fontSize = "70px";
+// tag.style.background = "yellow";
+// tag.style.marginTop = "200px";
+// tag.style.textAlign = "center";
+
+// var h1 = document.querySelector("h1");
+
+// h1.style.color = "yellow";
+// h1.style.border = "5px solid pink";
+
+var p = document.querySelector("p");
+
+p.classList.add("big")
+
+p.classList.remove("big")
+
+// p.classList.toggle("big")
+//TOGGLES ON!!!
+// p.classList.toggle("big")
+//TOGGLES OFF!!!
+
+p.classList.toggle("done")
