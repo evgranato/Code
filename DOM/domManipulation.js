c7f47f6ad9279863f5071f59030c5b1597aec4b0
Evan Granato <evgranato@gmail.com>
2020-06-30 15:21:36 -0500

    update

--- DOM/domManipulation.js
+++ DOM/domManipulation.js
@@ -14,15 +14,23 @@
 // h1.style.color = "yellow";
 // h1.style.border = "5px solid pink";
 
-var p = document.querySelector("p");
+// var p = document.querySelector("p");
 
-p.classList.add("big")
+// p.classList.add("big");
 
-p.classList.remove("big")
+// p.classList.remove("big");
 
 // p.classList.toggle("big")
 //TOGGLES ON!!!
 // p.classList.toggle("big")
 //TOGGLES OFF!!!
 
-p.classList.toggle("done")
+// p.classList.toggle("done");
+
+var p = document.getElementsByTagName("p")[0];
+
+var ul = document.querySelector("ul");
+
+// p.innerHTML;
+// SHOWS THE INNER HTML TAGS LIKE THE <STRONG> TAG IN THE <P>
+
