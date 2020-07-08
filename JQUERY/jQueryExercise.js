4ab8482d6ef08e0c2226e05bbeffb82d65cf4df6
Evan Granato <evgranato@gmail.com>
2020-07-08 15:44:45 -0500

    finished jQuery Exercise

---
+++ JQUERY/jQueryExercise.js
@@ -0,0 +1,7 @@
+$("div").css("backgroundColor", "purple");
+
+$("div.highlight").css("width", "200px");
+
+$("#third").css("border", " solid orange");
+
+$("div:first-of-type").css("color", "pink");
