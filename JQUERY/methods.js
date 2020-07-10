179e99d90b935382889a3a8a2f228c20ea044760
Evan Granato <evgranato@gmail.com>
2020-07-09 19:28:52 -0500

    jQuery Methods part 1

---
+++ JQUERY/methods.js
@@ -0,0 +1,7 @@
+$("h1").text("JQuery Rocks")
+
+// $("li:nth-of-type(2)").text("testing")
+
+// $("li").text("Colt's dog is adorable")
+
+$("ul").html("<li>I Hacked your UL!</li><li>Rusty is still adorable</li><li>Work it</li>")
