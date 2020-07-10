989a324ed955ad1013d11d9430fb883f75af65c3
Evan Granato <evgranato@gmail.com>
2020-07-10 09:05:40 -0500

    jQuery advanced almost complete

---
+++ JQUERY/jQueryEvents.js
@@ -0,0 +1,42 @@
+$("h1").css("width", "300px");
+
+// click()
+// $("h1").click(function() {
+//     alert("H1 Clicked")
+// });
+// $("button").click(function() {
+//     var text = $(this).text();
+//     console.log("You Clicked: " + text);
+//     $(this).css("background-color", "pink");
+// })
+
+// keypress()
+// $("input").keypress(function(evnet) {
+//     if(event.which ===13) {
+//         alert("YOU HIT ENTER")
+//     }
+//     else {
+//         console.log("NOT ENTER")
+//     }
+// })
+
+// on()
+$("input").on("keypress", function(evnet) {
+    if(event.which ===13) {
+        alert("YOU HIT ENTER")
+    }
+    else {
+        console.log("NOT ENTER")
+    }
+})
+
+$("h1").on("click", function() {
+    $(this).css("color", "purple");
+})
+
+$("button").on("mouseenter", function() {
+    $(this).css("font-weight", "bold");
+})
+$("button").on("mouseleave", function() {
+    $(this).css("font-weight", "normal");
+})
