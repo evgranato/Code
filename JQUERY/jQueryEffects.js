543a189005a002c5ad761749657e944d8dc076f4
Evan Granato <evgranato@gmail.com>
2020-07-12 12:48:33 -0500

    finished effects

---
+++ JQUERY/jQueryEffects.js
@@ -0,0 +1,17 @@
+// $("button").on("click", function() {
+//     $("div").fadeOut(1000, function() {
+//         $(this).remove();
+//     });
+// })
+
+// $("button").on("click", function() {
+//     $("div").fadeIn(1000);
+// })
+
+// $("button").on("click", function() {
+//     $("div").fadeToggle(1000);
+// })
+
+$("button").on("click", function() {
+    $("div").slideToggle(1000);
+})
