699047a14312fdfbe6b7d842139d6a65005f749b
Evan Granato <evgranato@gmail.com>
2020-07-14 15:46:05 -0500

    finished todo app

--- TODO App/assets/js/toDoApp.js
+++ TODO App/assets/js/toDoApp.js
@@ -13,6 +13,10 @@ $("input[type='text']").on("keypress", function(event) {
     if(event.which === 13) {
         var todoText = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>X</span> " + todoText +"</ul>");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +"</ul>");
     }
+})
+
+$(".fa-plus").on("click", function() {
+    $("input").fadeToggle();
 })
