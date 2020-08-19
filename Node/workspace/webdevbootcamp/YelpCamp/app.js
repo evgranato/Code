78a5db40aa2739eed8ae5315a1bfe6c98b54f2e5
Evan Granato <evgranato@gmail.com>
2020-08-19 15:35:13 -0500

    Yelpcamp continued

--- Node/workspace/webdevbootcamp/YelpCamp/app.js
+++ Node/workspace/webdevbootcamp/YelpCamp/app.js
@@ -10,8 +10,8 @@ app.get("/", function(req, res) {
 app.get("/campgrounds", function(req, res) {
     var campgrounds = [
         {name: "Salmon Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=350"},
-        {name: "Granite Hill", image: "https://pixabay.com/get/53e4d1424b56a814f1dc84609620367d1c3ed9e04e507440722c73d1914ac7_340.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507440722c73d1914ac7_340.jpg"}
+        {name: "Granite Hill", image: "https://blog-assets.thedyrt.com/uploads/2018/06/freecampingspot-2000x1120.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://i.insider.com/5ed13edd988ee326725f1526?width=1100&format=jpeg&auto=webp"}
     ]
 
     res.render("campgrounds", {campgrounds:campgrounds});
