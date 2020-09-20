30a4d49753894e26d2c942410e3763aee0dc9013
Evan Granato <evgranato@gmail.com>
2020-09-20 07:24:54 -0500

    blog example

--- Node/workspace/webdevbootcamp/RESTfulBlogApp/app.js
+++ Node/workspace/webdevbootcamp/RESTfulBlogApp/app.js
@@ -54,7 +54,16 @@ app.post("/blogs", function(req, res){
         }
     })
 })
-
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
 
 
 app.listen(3000, function() {
